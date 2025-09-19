5c33dbee4b086a1fc40a60bf7dfd6083a7a490fb
yadhunandan <yadhunandan2002@gmail.com>
2025-09-19 11:57:16 +0530

    updated url

--- src/main/java/com/MindMitra/Rolebased/Controller/AuthController.java
+++ src/main/java/com/MindMitra/Rolebased/Controller/AuthController.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "")
 @RequiredArgsConstructor
 public class AuthController {
 
